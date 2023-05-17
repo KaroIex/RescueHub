9e0974cc6933fd29c267b362008bfd85c2e542ed
your.user.name <mateuszgruszkiewicz@gmail.com>
2023-05-17 16:45:18 +0200

    spring web project init

---
+++ RescueHubProject/src/test/java/com/example/rescuehubproject/RescueHubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rescuehubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueHubProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
