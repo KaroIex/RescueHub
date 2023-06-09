3106b26911be272c6c1616aaa58216d753d6be3a
Anhelina Piatrovich <lina.petrovich.1998@gmail.com>
2023-06-09 20:07:00 +0200

    Poprawianie i aktualizacja crudAnimals

--- RescueHubProject/src/main/java/com/example/rescuehubproject/security/SecurityConfiguration.java
+++ RescueHubProject/src/main/java/com/example/rescuehubproject/security/SecurityConfiguration.java
@@ -79,12 +79,20 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET, "/api/auth/adopter").hasRole(ADOPTER)
 
                         //*****ANIMAL SPECIES*****\\
-                        .requestMatchers(HttpMethod.GET, "/api/animalspecies/{id}").permitAll()
+                      /*  .requestMatchers(HttpMethod.GET, "/api/animalspecies/{id}").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/animalspecies").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/animalspecies").hasRole(ADMIN)
                         .requestMatchers(HttpMethod.PUT, "/api/animalspecies/{id}").hasRole(ADMIN)
                         .requestMatchers(HttpMethod.DELETE, "/api/animalspecies/{id}").hasRole(ADMIN)
                         //*****ANIMAL SPECIES*****\\
+
+                        //*****ANIMAL *****\\
+                        .requestMatchers(HttpMethod.GET, "/api/animalspecies/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/animalspecies").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/animalspecies").hasRole(ADMIN)
+                        .requestMatchers(HttpMethod.PUT, "/api/animalspecies/{id}").hasRole(ADMIN)
+                        .requestMatchers(HttpMethod.DELETE, "/api/animalspecies/{id}").hasRole(ADMIN)
+                        //*****ANIMAL *****\\ */
 
                         .anyRequest().permitAll() // any other request for test purpose
                 )
