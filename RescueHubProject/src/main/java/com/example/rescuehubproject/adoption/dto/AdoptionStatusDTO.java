1e4842bf1dfea57942d76be1729e1fe1d409c51e
Anhelina Piatrovich <lina.petrovich.1998@gmail.com>
2023-06-18 16:59:13 +0200

    update AdoptionStatusDTO

--- RescueHubProject/src/main/java/com/example/rescuehubproject/adoption/dto/AdoptionStatusDTO.java
+++ RescueHubProject/src/main/java/com/example/rescuehubproject/adoption/dto/AdoptionStatusDTO.java
@@ -1,5 +1,6 @@
 package com.example.rescuehubproject.adoption.dto;
 
+import com.example.rescuehubproject.animals.dto.AnimalSpeciesDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -7,16 +8,15 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class AdoptionStatusDTO {
-
-    @NotEmpty(message = "Adopter's name must not be empty")
-    @Schema(description = "Adopter's name", example = "John Doe")
-    private String adopterName;
-
-    @NotEmpty(message = "Animal's name must not be empty")
-    @Schema(description = "Animal's name", example = "Bobby")
-    private String animalName;
+public class AdoptionStatusDTO extends AdoptionDTO {
 
     @NotEmpty(message = "Status must not be empty")
-    private String Status;
+    private String status;
+
+    public AdoptionStatusDTO(String adoptername, String animalName){
+        super(adoptername, animalName);
+        this.status = status;
+    }
+
+    AdoptionStatusDTO(){}
 }
