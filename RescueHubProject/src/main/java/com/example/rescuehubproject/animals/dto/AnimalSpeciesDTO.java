7e2b59b441b5c7d0633b5f61c25b1751db8c57f8
KaroIex <karolkula11@gmail.com>
2023-06-20 01:13:17 +0200

    saving progress

--- RescueHubProject/src/main/java/com/example/rescuehubproject/animals/dto/AnimalSpeciesDTO.java
+++ RescueHubProject/src/main/java/com/example/rescuehubproject/animals/dto/AnimalSpeciesDTO.java
@@ -1,5 +1,6 @@
 package com.example.rescuehubproject.animals.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,8 +8,10 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(name = "AnimalSpeciesDTO", description = "DTO representing an animal species")
 public class AnimalSpeciesDTO {
 
+    @Schema(description = "Species name", example = "Dog")
     @NotEmpty(message = "Species name must not be empty")
     private String speciesName;
 
